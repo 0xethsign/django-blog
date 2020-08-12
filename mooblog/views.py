008c800e405ea370da57adfe840c3183dc039c7c
smrpn <samarpan_d@pp.iitr.ac.in>
2020-08-12 14:22:10 +0530

    something's wrong

---
+++ mooblog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import FormPost, FormEdit
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-id']
+    
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'articles_detail.html'
+    
+class AddPostView(CreateView):
+    model = Post
+    form_class = FormPost
+    template_name = 'add_post.html'
+    #fields = ('title','author','body')
+    
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = FormEdit
+    template_name = 'update_post.html'
+    # fields = '__all__'
+    
+    
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
